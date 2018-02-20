b8e5346e096e6d8758bf281eab07200a04e9834e
Inka <inka.pelin13@gmail.com>
2018-02-20 22:50:39 +0100

    Updates

--- src/reducers/startdate.js
+++
@@ -1,9 +0,0 @@
-function startdate(state='', action) {
-	switch(action.type) {
-		case 'GOT_STARTDATE':
-			return action.date;
-		default:
-		 	return state;	
-	}
-}
-export default startdate;
--- src/reducers/symbol.js
+++
@@ -1,9 +0,0 @@
-function symbol(state='', action) {
-	switch(action.type) {
-		case 'GOT_SYMBOL':
-			return action.symbol;
-		default:
-		 	return state;	
-	}
-}
-export default symbol;
